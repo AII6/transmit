74b214988b1f12dd9bfcac442485820322a18517
Chaojie <1791828150@qq.com>
2022-07-05 09:29:32 +0800

    serial send

--- main.py
+++ main.py
@@ -4,29 +4,15 @@ sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
 
 image = cv2.imread("C:\\Users\\Jack Lee\\Pictures\\lcj34.jpg")
-#
-# print(image.dtype)
-# print(len(image.reshape(-1, 1)))
+
 video = cv2.VideoCapture('/dev/video0')
 s = socket.socket()
 s.connect(('192.168.1.100', 30000))
 while True:
     success, frame = video.read()
-    # print(frame.dtype)
-    # print(frame.shape)
-    # print(len(frame.reshape(-1, 1)))
+
     if success:
-        # cv2.imshow("11", frame)
-        # a = base64.b64encode(bytes(frame))
-        # for i in range(480):
-        #     # cv2.waitKey(100)
-        #     tlen = s.send(frame[i])
-        #     if tlen != 1920:
-        #         print(tlen)
+
         s.sendall(frame)
-        # print(np.sum(frame))
-        # break
-    # cv2.waitKey(100)
-    # s.sendall(image)
-    # print(image.shape)
+
 
---
+++ mySerial.py
@@ -0,0 +1,29 @@
+import serial
+import sys
+
+# class mySerial(serial.Serial):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         ser = serial.Serial()
+#         ser.port = "COM4"  # 设置端口号
+#         ser.baudrate = 115200  # 设置波特率
+#         ser.bytesize = 8  # 设置数据位
+#         ser.stopbits = 1  # 设置停止位
+#         ser.parity = "N"  # 设置校验位
+#         ser.open()  # 打开串口,要找到对的串口号才会成功
+
+ser = serial.Serial()
+ser.port = "COM4"  # 设置端口号
+ser.baudrate = 115200  # 设置波特率
+ser.bytesize = 8  # 设置数据位
+ser.stopbits = 1  # 设置停止位
+ser.parity = "N"  # 设置校验位
+# ser.open()  # 打开串口,要找到对的串口号才会成功
+
+
+def quit(signum, frame):
+    data = "999\r\n"
+    for i in range(5):
+        ser.write(data.encode('gbk'))
+    print("exit!!!")
+    sys.exit(0)     # 手动终止程序
